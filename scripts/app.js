4aa4cba7bd0c8294808172721743a1a4a9a6074a
Michael Shinners <mishinners@icloud.com>
2017-09-17 12:29:46 -0700

    linked projects and imgs

--- scripts/app.js
+++ scripts/app.js
@@ -19,12 +19,12 @@ $('.main h1').hide().fadeIn(3000);
 var titleArray = ['UX/UI Designer & Developer', 'Software Developer', 'Web Designer & Developer', 'Software Engineer', 'Dev'];
 var currentTitleIndex = 0;
   for (var i = 0; i < titleArray.length; i++) {
-// var titleInterval = setInterval(function(){
-//   ++currentTitleIndex;
-//   if (currentTitleIndex >= titleArray.length) {
-//     currentTitleIndex = 0;
-//   }
-// });
+var titleInterval = setInterval(function(){
+  ++currentTitleIndex;
+  if (currentTitleIndex >= titleArray.length) {
+    currentTitleIndex = 0;
+  }
+});
 $('.jobName').text(titleArray[i]).fadeIn(4000).fadeOut(2000);
 }
 // }, 1000);
@@ -46,16 +46,7 @@ $('#contactButton').click(function(){
   $("#bio").fadeOut(1000);
   $("#projectHeader").fadeOut(1000);
 });
-//Future work
-// $( "#about" ).onClick();
-// $( "#portfolio" ).onClick();
-// $( "#contact" ).onClick();
-//
-// $('#title').show()
-// $('#bio').show()
-// $('#projectHeader').show()
-//
-// $('#linkedIn').onClick()
-// $('#github').onClick()
+
+
 
 // .toHtml()
