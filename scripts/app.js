114e040b06774bd5900d973dc70efd4e0a39d1d9
Michael Shinners <mishinners@icloud.com>
2017-09-25 12:25:32 -0700

    put my code into an IIFE

--- scripts/app.js
+++ scripts/app.js
@@ -1,65 +1,71 @@
 'use strict';
+var app = app || {};
 
-//function to put projects in a grid
-var projects = [];
+  (function (module) {
 
-var projectTemplateHTML = $('#projectTemplate').html();
-// console.log(projectTemplateHTML);
-var templateFunction = Handlebars.compile(projectTemplateHTML);
-// console.log(templateFunction);
-function Portfolio(projectData) {
-  Object.assign(this, projectData);
-  projects.push(this);
-  // console.log(this);
-};
-// console.log(projectData);
-Portfolio.prototype.toHtml = function() {
-  var templateParts = templateFunction(this);
-  $('#projectsDiv').append(templateParts);
+    var articleView = {};
+  //function to put projects in a grid
+  var projects = [];
 
-  // console.log(templateParts);
-}
-projectData.forEach(function(projObj) {
-  new Portfolio(projObj);
-});
-projects.forEach(function(projObj) {
-  projObj.toHtml();
-});
+  var projectTemplateHTML = $('#projectTemplate').html();
+  // console.log(projectTemplateHTML);
+  var templateFunction = Handlebars.compile(projectTemplateHTML);
+  // console.log(templateFunction);
+  function Portfolio(projectData) {
+    Object.assign(this, projectData);
+    projects.push(this);
+    // console.log(this);
+  };
+  // console.log(projectData);
+  Portfolio.prototype.toHtml = function() {
+    var templateParts = templateFunction(this);
+    $('#projectsDiv').append(templateParts);
 
-$('.main h1').hide().fadeIn(2000);
-
-//function to loop through job titles
-var titleArray = ['Software Developer', 'Web Designer & Developer', 'Software Engineer', 'Data Scientist', 'UX/UI  Developer'];
-var currentTitleIndex = 0;
-var titleInterval = setInterval(function(){
-  for (var i = 0; i < titleArray.length; i++) {
-  currentTitleIndex++;
-  if (currentTitleIndex >= titleArray.length) {
-    currentTitleIndex = 0;
+    // console.log(templateParts);
   }
-}
-$('.jobName').text(titleArray[i]).fadeIn(2000).fadeOut(1500);
-});
+  projectData.forEach(function(projObj) {
+    new Portfolio(projObj);
+  });
+  projects.forEach(function(projObj) {
+    projObj.toHtml();
+  });
 
-//Function chain that allows different elements to fade-in/out
-$('#aboutButton').click(function(){
-  $("#bio").fadeIn(2000);
-  $("#linkedIn").fadeOut(500);
-  $("#github").fadeOut(500);
-  $("#projectsDiv").fadeOut(500);
-});
-$('#projectsButton').on('click', function(){
-  $("#projectsDiv").fadeIn(2000);
-  console.log($('#projectsDiv')[0]);
-  $("#bio").fadeOut(500);
-  $("#linkedIn").fadeOut(500);
-  $("#github").fadeOut(500);
-});
-$('#contactButton').click(function(){
-  $("#linkedIn").fadeIn(2000);
-  $("#github").fadeIn(2000);
-  $("#bio").fadeOut(500);
-  $("#projectsDiv").fadeOut(500);
-});
+  $('.main h1').hide().fadeIn(2000);
 
-$('#hamMenu').click()
+  //function to loop through job titles
+  var titleArray = ['Software Developer', 'Web Designer & Developer', 'Software Engineer', 'Data Scientist', 'UX/UI  Developer'];
+  var currentTitleIndex = 0;
+  var titleInterval = setInterval(function(){
+    for (var i = 0; i < titleArray.length; i++) {
+    currentTitleIndex++;
+    if (currentTitleIndex >= titleArray.length) {
+      currentTitleIndex = 0;
+    }
+  }
+  $('.jobName').text(titleArray[i]).fadeIn(2000).fadeOut(1500);
+  });
+
+  //Function chain that allows different elements to fade-in/out
+  $('#aboutButton').click(function(){
+    $("#bio").fadeIn(2000);
+    $("#linkedIn").fadeOut(500);
+    $("#github").fadeOut(500);
+    $("#projectsDiv").fadeOut(500);
+  });
+  $('#projectsButton').on('click', function(){
+    $("#projectsDiv").fadeIn(2000);
+    console.log($('#projectsDiv')[0]);
+    $("#bio").fadeOut(500);
+    $("#linkedIn").fadeOut(500);
+    $("#github").fadeOut(500);
+  });
+  $('#contactButton').click(function(){
+    $("#linkedIn").fadeIn(2000);
+    $("#github").fadeIn(2000);
+    $("#bio").fadeOut(500);
+    $("#projectsDiv").fadeOut(500);
+  });
+
+  $('#hamMenu').click()
+  app.articleView = articleView;
+})(app);
